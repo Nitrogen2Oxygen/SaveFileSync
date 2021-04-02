9524c98c94e95be6a56ff1eb22d012267835468b
Nitrogen2Oxygen <nitrogenandoxygen@gmail.com>
2021-04-02 10:24:55 -0400

    Create a Save.toZipFile function

--- src/main/java/com/github/nitrogen2oxygen/SaveFileSync/data/client/Save.java
+++ src/main/java/com/github/nitrogen2oxygen/SaveFileSync/data/client/Save.java
@@ -1,6 +1,16 @@
 package com.github.nitrogen2oxygen.SaveFileSync.data.client;
 
+import com.github.nitrogen2oxygen.SaveFileSync.utils.FileUtils;
+
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Save {
     public File file;
@@ -9,5 +19,36 @@ public class Save {
     public Save(String name, File file) {
         this.name = name;
         this.file = file;
+    }
+
+    public byte[] toZipFile() throws Exception {
+        // Write to a temporary directory
+        File tmpdir = Files.createTempDirectory("SaveFileSync").toFile();
+        File tmpfile = new File(tmpdir, name + ".zip");
+        byte[] buffer = new byte[1024];
+        FileOutputStream fos = new FileOutputStream(tmpfile);
+        ZipOutputStream zos = new ZipOutputStream(fos);
+
+        // Get file list
+        List<String> fileList = FileUtils.generateFileList(file);
+        for (String file : fileList) {
+            String subdir = file.substring(this.file.getPath().length() + 1);
+            ZipEntry ze = new ZipEntry(name + File.separator + subdir);
+            zos.putNextEntry(ze);
+            FileInputStream in = new FileInputStream(file);
+            int len;
+            while ((len = in.read(buffer)) > 0) {
+                zos.write(buffer, 0, len);
+            }
+            in.close();
+            zos.closeEntry();
+        }
+        zos.close();
+        fos.close();
+
+        // Delete the temp directory and return the result
+        byte[] res = Files.readAllBytes(tmpfile.toPath());
+        org.apache.commons.io.FileUtils.deleteDirectory(tmpdir);
+        return res;
     }
 }
---
+++ src/main/java/com/github/nitrogen2oxygen/SaveFileSync/utils/FileUtils.java
@@ -0,0 +1,25 @@
+package com.github.nitrogen2oxygen.SaveFileSync.utils;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtils {
+    public static List<String> generateFileList(File node) {
+        List<String> fileList = new ArrayList<>();
+        // add file only
+        if (node.isFile()) {
+            fileList.add(node.toString());
+        }
+
+        if (node.isDirectory()) {
+            String[] subNode = node.list();
+            assert subNode != null;
+            for (String filename: subNode) {
+                List<String> subList = generateFileList(new File(node, filename));
+               fileList.addAll(subList);
+            }
+        }
+        return fileList;
+    }
+}
