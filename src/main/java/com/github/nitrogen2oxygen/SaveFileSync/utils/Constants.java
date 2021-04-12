8fbe9d6ac9a51947a9373d1f7a18c3d4314f2768
Nitrogen2Oxygen <nitrogenandoxygen@gmail.com>
2021-04-12 11:47:00 -0400

    Pushed to v0.3.1-alpha

--- src/main/java/com/github/nitrogen2oxygen/SaveFileSync/utils/Constants.java
+++ src/main/java/com/github/nitrogen2oxygen/SaveFileSync/utils/Constants.java
@@ -3,7 +3,7 @@ package com.github.nitrogen2oxygen.SaveFileSync.utils;
 import java.nio.file.Paths;
 
 public class Constants {
-    public static final String VERSION = "v0.3.0-alpha";
+    public static final String VERSION = "v0.3.1-alpha";
     public static final String APP_NAME = "Save File Sync";
     public static final String DROPBOX_APP_ID = "i136jjbqxg4aaci";
 
