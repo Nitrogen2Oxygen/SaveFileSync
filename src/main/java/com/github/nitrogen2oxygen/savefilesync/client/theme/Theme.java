3b7ec49932c74bc517321ba97e805ce779e0da2c
Nitrogen2Oxygen <nitrogenandoxygen@gmail.com>
2021-04-18 20:51:24 -0400

    Added several new flatlaf themes

--- src/main/java/com/github/nitrogen2oxygen/savefilesync/client/theme/Theme.java
+++ src/main/java/com/github/nitrogen2oxygen/savefilesync/client/theme/Theme.java
@@ -2,5 +2,10 @@ package com.github.nitrogen2oxygen.savefilesync.client.theme;
 
 public enum Theme {
     DARK,
-    LIGHT
+    LIGHT,
+    MATERIAL_DARK,
+    MATERIAL_OCEAN,
+    MATERIAL_LIGHT,
+    CARBON,
+    ONE_DARK
 }
--- src/main/java/com/github/nitrogen2oxygen/savefilesync/client/theme/Themes.java
+++ src/main/java/com/github/nitrogen2oxygen/savefilesync/client/theme/Themes.java
@@ -1,6 +1,8 @@
 package com.github.nitrogen2oxygen.savefilesync.client.theme;
 
 import com.formdev.flatlaf.*;
+import com.formdev.flatlaf.intellijthemes.FlatCarbonIJTheme;
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.*;
 
 import java.awt.*;
 
@@ -16,6 +18,16 @@ public class Themes {
                 return "Dark";
             case LIGHT:
                 return "Light";
+            case MATERIAL_DARK:
+                return "Material Dark";
+            case MATERIAL_OCEAN:
+                return "Material Oceanic";
+            case MATERIAL_LIGHT:
+                return "Material Lighter";
+            case CARBON:
+                return "Carbon";
+            case ONE_DARK:
+                return "One Dark";
             default:
                 return null;
         }
@@ -27,6 +39,16 @@ public class Themes {
                 return new FlatDarkLaf();
             case LIGHT:
                 return new FlatLightLaf();
+            case MATERIAL_DARK:
+                return new FlatMaterialDarkerIJTheme();
+            case MATERIAL_OCEAN:
+                return new FlatMaterialOceanicIJTheme();
+            case MATERIAL_LIGHT:
+                return new FlatMaterialLighterIJTheme();
+            case CARBON:
+                return new FlatCarbonIJTheme();
+            case ONE_DARK:
+                return new FlatAtomOneDarkIJTheme();
             default:
                 return null;
         }
@@ -47,6 +69,7 @@ public class Themes {
                     case WARNING:
                         return Color.YELLOW;
                 }
+                break;
             case LIGHT:
                 switch (color) {
                     case ERROR:
@@ -60,6 +83,77 @@ public class Themes {
                     case WARNING:
                         return new Color(218, 165, 32);
                 }
+                break;
+            case MATERIAL_DARK:
+                switch (color) {
+                    case ERROR:
+                        return Color.ORANGE;
+                    case DEFAULT:
+                        return Color.WHITE;
+                    case OFFLINE:
+                        return Color.RED;
+                    case SUCCESS:
+                        return Color.GREEN;
+                    case WARNING:
+                        return Color.YELLOW;
+                }
+                break;
+            case MATERIAL_OCEAN:
+                switch (color) {
+                    case ERROR:
+                        return Color.ORANGE;
+                    case DEFAULT:
+                        return Color.WHITE;
+                    case OFFLINE:
+                        return Color.RED;
+                    case SUCCESS:
+                        return Color.GREEN;
+                    case WARNING:
+                        return Color.YELLOW;
+                }
+                break;
+            case MATERIAL_LIGHT:
+                switch (color) {
+                    case ERROR:
+                        return new Color(255, 140, 0);
+                    case DEFAULT:
+                        return Color.BLACK;
+                    case OFFLINE:
+                        return Color.RED;
+                    case SUCCESS:
+                        return new Color(0, 150, 0);
+                    case WARNING:
+                        return new Color(218, 165, 32);
+                }
+                break;
+            case CARBON:
+                switch (color) {
+                    case ERROR:
+                        return Color.ORANGE;
+                    case DEFAULT:
+                        return Color.WHITE;
+                    case OFFLINE:
+                        return Color.RED;
+                    case SUCCESS:
+                        return Color.GREEN;
+                    case WARNING:
+                        return Color.YELLOW;
+                }
+                break;
+            case ONE_DARK:
+                switch (color) {
+                    case ERROR:
+                        return Color.ORANGE;
+                    case DEFAULT:
+                        return Color.WHITE;
+                    case OFFLINE:
+                        return Color.RED;
+                    case SUCCESS:
+                        return Color.GREEN;
+                    case WARNING:
+                        return Color.YELLOW;
+                }
+                break;
         }
         return null;
     }
