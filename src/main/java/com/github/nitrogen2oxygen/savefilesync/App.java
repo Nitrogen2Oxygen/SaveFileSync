3bdd6ac8d83514eea8d2534bff16de91f2fc5a85
Nitrogen2Oxygen <nitrogenandoxygen@gmail.com>
2021-04-23 06:42:12 -0400

    Remove progress bar

--- src/main/java/com/github/nitrogen2oxygen/savefilesync/App.java
+++ src/main/java/com/github/nitrogen2oxygen/savefilesync/App.java
@@ -1,7 +1,6 @@
 package com.github.nitrogen2oxygen.savefilesync;
 
 import com.github.nitrogen2oxygen.savefilesync.client.ClientData;
-import com.github.nitrogen2oxygen.savefilesync.ui.component.ProgressBar;
 import com.github.nitrogen2oxygen.savefilesync.util.Constants;
 import com.github.nitrogen2oxygen.savefilesync.util.DataManager;
 import com.github.nitrogen2oxygen.savefilesync.ui.MainPanel;
@@ -15,44 +14,36 @@ import java.io.File;
 public class App {
 
     public static void main(String[] args) {
-        /* Create a progress bar */
-        try (ProgressBar pb = new ProgressBar("Loading " + Constants.APP_NAME + "...")) {
-            pb.start();
-            /* Obtain the client data from <USER_HOME>/SaveFileSync */
-            File clientDataFolder = new File(FileLocations.getDataDirectory());
-            if (!clientDataFolder.isDirectory() || !clientDataFolder.exists()) {
-                boolean mkdir = clientDataFolder.mkdir();
-                if (!mkdir) {
-                    JOptionPane.showMessageDialog(null,
-                            "Cannot create data folder!", "Error!", JOptionPane.ERROR_MESSAGE); // In case the directory can't be made for some reason, throw an error to the user
-                    System.exit(1);
-                }
+        /* Obtain the client data from <USER_HOME>/SaveFileSync */
+        File clientDataFolder = new File(FileLocations.getDataDirectory());
+        if (!clientDataFolder.isDirectory() || !clientDataFolder.exists()) {
+            boolean mkdir = clientDataFolder.mkdir();
+            if (!mkdir) {
+                JOptionPane.showMessageDialog(null,
+                        "Cannot create data folder!", "Error!", JOptionPane.ERROR_MESSAGE); // In case the directory can't be made for some reason, throw an error to the user
+                System.exit(1);
             }
-            pb.setPercent(25);
-
-            /* Create the data object. This object stores any kind of persistent data on the client */
-            ClientData data = DataManager.load();
-            data.getSettings().apply(); // Apply the settings object at startup
-            pb.setPercent(50);
-
-            /* Finally, creating the actual UI frame. Communication between front and backend is iffy but we make do */
-            JFrame frame = new JFrame(Constants.APP_NAME + " - " + Constants.VERSION);
-            frame.setContentPane(new MainPanel(data).getRootPanel()); // The UI required a ClientData object to update the lists and such
-            frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-            frame.addWindowListener(new WindowAdapter() {
-                @Override
-                public void windowClosing(WindowEvent e) {
-                    DataManager.save(data);
-                    System.exit(0);
-                }
-            });
-            frame.pack();
-            frame.setLocationRelativeTo(null);
-            pb.setPercent(75);
-
-            /* Set the frame visible and load the UI */
-            frame.setVisible(true);
-            pb.setPercent(100);
         }
+
+        /* Create the data object. This object stores any kind of persistent data on the client */
+        ClientData data = DataManager.load();
+        data.getSettings().apply(); // Apply the settings object at startup
+
+        /* Finally, creating the actual UI frame. Communication between front and backend is iffy but we make do */
+        JFrame frame = new JFrame(Constants.APP_NAME + " - " + Constants.VERSION);
+        frame.setContentPane(new MainPanel(data).getRootPanel()); // The UI required a ClientData object to update the lists and such
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                DataManager.save(data);
+                System.exit(0);
+            }
+        });
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+
+        /* Set the frame visible and load the UI */
+        frame.setVisible(true);
     }
 }
