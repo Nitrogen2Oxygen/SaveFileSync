d3421d84195b853623e32bb260c26d11fa529d84
Nitrogen2Oxygen <nitrogenandoxygen@gmail.com>
2021-06-13 21:06:27 -0400

    Added back save functionality

--- src/main/java/com/github/nitrogen2oxygen/savefilesync/save/SaveDirectory.java
+++ src/main/java/com/github/nitrogen2oxygen/savefilesync/save/SaveDirectory.java
@@ -1,8 +1,18 @@
 package com.github.nitrogen2oxygen.savefilesync.save;
 
+import com.github.nitrogen2oxygen.savefilesync.util.FileUtilities;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class SaveDirectory extends Save {
     public SaveDirectory(String name, File file) {
@@ -11,12 +21,78 @@ public class SaveDirectory extends Save {
 
     @Override
     public byte[] toZipData() throws Exception {
-        return new byte[0];
+        File file = getFile();
+        String name = getName();
+        if (!file.exists() || FileUtilities.generateFileList(file).isEmpty())
+            return new byte[0];
+
+        /* Create a temporary zip file */
+        File tmpFile = Files.createTempFile("SaveFileSync", ".zip").toFile();
+        tmpFile.deleteOnExit();
+
+        /* Write to the file */
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(tmpFile));
+        List<String> fileList = FileUtilities.generateFileList(file);
+        for (String saveFile : fileList) {
+            String subDir = saveFile.substring(file.getPath().length() + 1);
+            ZipEntry entry = new ZipEntry(name + File.separator + subDir);
+            out.putNextEntry(entry);
+            byte[] data = FileUtils.readFileToByteArray(new File(saveFile));
+            out.write(data);
+            out.closeEntry();
+        }
+
+        /* Cleanup */
+        out.close();
+        byte[] data = Files.readAllBytes(tmpFile.toPath());
+        tmpFile.delete();
+
+        /* Return the result */
+        return data;
     }
 
     @Override
     public void writeData(byte[] data, boolean makeBackup, boolean forceOverwrite) throws Exception {
+        File file = getFile();
+        String name = getName();
 
+        /* Backup our current data if the user wants to */
+        if (makeBackup) FileUtilities.makeBackup(this);
+
+        /* Create temporary file to download the zip file from */
+        File tmpFile = Files.createTempFile("SaveFileSync", ".zip").toFile();
+        tmpFile.deleteOnExit();
+        FileUtils.writeByteArrayToFile(tmpFile, data);
+
+        /* Delete data if force overwrite is enabled */
+        if (forceOverwrite) FileUtils.cleanDirectory(file);
+
+        /* Extract zip to original folder */
+        ZipInputStream in = new ZipInputStream(new FileInputStream(tmpFile));
+        ZipEntry entry = in.getNextEntry();
+
+        /* Is a folder */
+        while (entry != null) {
+            String filePath = file.getPath() + File.separator + entry.getName().substring(name.length() + 1);
+            File saveFile = new File(filePath);
+            if (!saveFile.toPath().normalize().startsWith(file.getPath())) // Test if the file name is valid due to bug
+                throw new Exception("Bad Zip Entry! Aborting!");
+            if (!entry.isDirectory()) {
+                saveFile.getParentFile().mkdirs();
+                saveFile.createNewFile();
+                IOUtils.copy(in, new FileOutputStream(saveFile));
+            } else {
+                File dir = new File(filePath);
+                dir.mkdirs();
+            }
+
+            in.closeEntry();
+            entry = in.getNextEntry();
+        }
+
+        /* Cleanup */
+        in.close();
+        tmpFile.delete();
     }
 
     @Override
--- src/main/java/com/github/nitrogen2oxygen/savefilesync/save/SaveFile.java
+++ src/main/java/com/github/nitrogen2oxygen/savefilesync/save/SaveFile.java
@@ -1,8 +1,17 @@
 package com.github.nitrogen2oxygen.savefilesync.save;
 
+import com.github.nitrogen2oxygen.savefilesync.util.FileUtilities;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.json.JSONObject;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
 
 public class SaveFile extends Save {
     public SaveFile(String name, File file) {
@@ -11,12 +20,57 @@ public class SaveFile extends Save {
 
     @Override
     public byte[] toZipData() throws Exception {
-        return new byte[0];
+        File file = getFile();
+        if (!file.exists())
+            return new byte[0];
+
+        /* Create a temporary zip file */
+        File tmpFile = Files.createTempFile("SaveFileSync", ".zip").toFile();
+        tmpFile.deleteOnExit();
+
+        /* Write to the file */
+        ZipOutputStream out = new ZipOutputStream(new FileOutputStream(tmpFile));
+        ZipEntry entry = new ZipEntry(file.getName());
+        out.putNextEntry(entry);
+        byte[] data = FileUtils.readFileToByteArray(file);
+        out.write(data);
+        out.closeEntry();
+
+        /* Cleanup */
+        out.close();
+        byte[] saveData = Files.readAllBytes(tmpFile.toPath());
+        tmpFile.delete();
+
+        /* Return the result */
+        return saveData;
     }
 
     @Override
     public void writeData(byte[] data, boolean makeBackup, boolean forceOverwrite) throws Exception {
+        File file = getFile();
 
+        /* Backup our current data if the user wants to */
+        if (makeBackup) FileUtilities.makeBackup(this);
+
+        /* Create temporary file to download the zip file from */
+        File tmpFile = Files.createTempFile("SaveFileSync", ".zip").toFile();
+        tmpFile.deleteOnExit();
+        FileUtils.writeByteArrayToFile(tmpFile, data);
+
+        /* Extract zip to original folder */
+        ZipInputStream in = new ZipInputStream(new FileInputStream(tmpFile));
+        ZipEntry entry = in.getNextEntry();
+
+        /* Is just a file, just overwrite the data */
+        String filePath = file.getPath();
+        File saveFile = new File(filePath);
+        if (!saveFile.toPath().normalize().startsWith(file.getPath())) // Test if the file name is valid due to bug
+            throw new Exception("Bad Zip Entry! Aborting!");
+        IOUtils.copy(in, new FileOutputStream(saveFile));
+
+        /* Cleanup */
+        in.close();
+        tmpFile.delete();
     }
 
     @Override
